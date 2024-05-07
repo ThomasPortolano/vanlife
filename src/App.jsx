716f575b9a47af3c41351f1702a0406ba0637deb
Thomas Portolano <tportolano@christies.com>
2024-05-07 15:30:34 +0800

    Fixing navbar links

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import AboutIndex from "./About/AboutIndex";
 function App() {
   return (
     <BrowserRouter>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutIndex />} />
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,12 +1,19 @@
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div className="navbar__container">
-      <div className="navbar__logo">#VANLIFE</div>
+      <div className="navbar__logo">
+        <Link to="/">#VANLIFE</Link>
+      </div>
       <div className="navbar__menu">
-        <div className="navbar__menu-item">About</div>
-        <div className="navbar__menu-item">Vans</div>
+        <Link className="navbar__menu-item" to="/about">
+          About
+        </Link>
+        <Link className="navbar__menu-item" to="/vans">
+          Vans
+        </Link>
       </div>
     </div>
   );
