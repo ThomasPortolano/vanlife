9433ab4b4c75fcaa53742cba60d98f464c4e7891
Thomas Portolano <tportolano@christies.com>
2024-05-13 22:55:54 +0800

    VL#11 - Implementing searchParams
     Using it for filtering van types

--- src/Pages/Vans/Vans.jsx
+++ src/Pages/Vans/Vans.jsx
@@ -1,9 +1,11 @@
 import "../../Styles/Vans.css";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 export default function Vans() {
   const [vans, setVans] = React.useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const typeFilter = searchParams.get("type");
 
   React.useEffect(() => {
     fetch("/api/vans")
@@ -11,7 +13,11 @@ export default function Vans() {
       .then((data) => setVans(data.vans));
   }, []);
 
-  const vanElements = vans.map((van) => {
+  const filteredVans = typeFilter
+    ? vans.filter((van) => van.type === typeFilter)
+    : vans;
+
+  const vanElements = filteredVans.map((van) => {
     const upperCaseType = van.type.charAt(0).toUpperCase() + van.type.slice(1);
 
     return (
@@ -37,6 +43,30 @@ export default function Vans() {
   return (
     <div className="van__container">
       <div className="van__title">Explore our van options</div>
+      <div className="van__filters__container">
+        <button
+          className="van__filters"
+          onClick={() => setSearchParams({ type: "simple" })}
+        >
+          Simple
+        </button>
+        <button
+          className="van__filters"
+          onClick={() => setSearchParams({ type: "luxury" })}
+        >
+          Luxury
+        </button>
+        <button
+          className="van__filters"
+          onClick={() => setSearchParams({ type: "rugged" })}
+        >
+          Rugged
+        </button>
+        <button className="van__clear" onClick={() => setSearchParams({})}>
+          Clear filter
+        </button>
+      </div>
+
       <div className="van__list">{vanElements}</div>
     </div>
   );
