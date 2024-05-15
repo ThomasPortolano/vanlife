753547bd929256a76cf59d9252b4355b8d292e1c
Thomas Portolano <tportolano@christies.com>
2024-05-15 09:12:18 +0800

    VL#13 - 404 styling

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+import "../index.css";
 
 export default function NotFound() {
   return (
-    <>
+    <div className="notfound__container">
       <h1>Sorry, the page you were looking for was not found.</h1>
-      <Link to="/">Return to Home</Link>
-    </>
+      <Link className="return__home" to="/">
+        Return to Home
+      </Link>
+    </div>
   );
 }
